bf97eed574e468a3bf8e9391da80ffe0e4569903
angelica espinosa roy <ariadna.espiroy@gmail.com>
2022-05-01 11:42:33 -0500

    Se creo tests para agregar UserView

--- test/UserView.test.js
+++ test/UserView.test.js
@@ -5,5 +5,15 @@ describe("Test for UserView", () => {
         const payload=null
         const result=UserView.createUser(payload)
         expect(result.error).toMatch(/payload no existe/)
-    }) 
+    });
+    test("Return an error object when try to create a new user with a palayload with invalid properties", () => {
+        const payload={username:null, name:12, id:"id"}
+        const result=UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    });
+    test("Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload={username: "Username"}
+        const result=UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    }); 
 });
